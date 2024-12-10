6417c9f22d27661a2708a2069b06471cae289eda
Jhosuan Ali Gonzalez <108597369+jhosuan15@users.noreply.github.com>
2024-12-10 13:20:29 -0600

    Update serverApi.py

--- serverApi.py
+++ serverApi.py
@@ -50,7 +50,8 @@ def insertar_transaccion():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.100.130', port=5002)  # Cambia host según la IP deseada
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 
