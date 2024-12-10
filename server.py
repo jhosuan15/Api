d84622ff1da91eb90d84860d2774c463997510f0
Jhosuan Ali Gonzalez <108597369+jhosuan15@users.noreply.github.com>
2024-12-10 12:26:02 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -6,20 +7,16 @@ app = Flask(__name__)
 CORS(app)
 
 try:
-    # Conexión a MongoDB
+    # Conexión a MongoDB Atlas (ya configurada con la URI)
     client = MongoClient("mongodb+srv://Jhosuan:Jhosuan@cluster.ftalklb.mongodb.net/Sistema", 
                          tls=True, tlsAllowInvalidCertificates=True)
-    
-    # Verificar la conexión
-    client.server_info()  # Esto lanzará un error si la conexión falla
+    client.server_info()
     print("Conexión exitosa a la base de datos MongoDB")
-
 except Exception as e:
     print(f"Error al conectar con la base de datos: {e}")
 
 db = client['Sistema']
 
-# Importar y registrar los blueprints
 from routes.inventarioRoutes import inventario_blueprint
 from routes.facturasRoutes import facturas_blueprint
 from routes.clientes import clientes_blueprint
@@ -29,8 +26,7 @@ app.register_blueprint(facturas_blueprint, url_prefix='/api/facturas')
 app.register_blueprint(clientes_blueprint, url_prefix='/api/clientes')
 app.register_blueprint(usuarios_blueprint, url_prefix='/api/usuarios')
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.100.130', port=5003)  # Cambia host según la IP deseada
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
